d477e3a5f42814804040fbfd3959c10d9b599d9c
Grace Buttler <grace.buttler@gmail.com>
2025-01-11 18:04:06 +0000

    write tests for counter component

--- frontend-stitch-counter/src/tests/counter.test.jsx
+++ frontend-stitch-counter/src/tests/counter.test.jsx
@@ -7,4 +7,21 @@ describe('Counter', () => {
 
         // screen.debug(); 
     })
+
+    it('should render three buttons with correct text inside the counter component', () => {
+        render(<Counter />)
+
+        const buttons = screen.getAllByRole('button')
+
+        expect(buttons[0]).toHaveTextContent('+')
+        expect(buttons[1]).toHaveTextContent('-')
+        expect(buttons[2]).toHaveTextContent('Reset')
+
+    })
+
+    
+    test.todo('+ button should add one to the counter')
+    test.todo('- button should subtract one from the counter')
+    test.todo('reset button should set counter to zero')
 })
+
