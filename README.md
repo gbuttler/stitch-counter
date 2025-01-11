ab4fa04b09668a7615d34c65f0536e7ae1f894b6
Grace Buttler <grace.buttler@gmail.com>
2025-01-11 13:05:11 +0000

    complete initial counter app, CSS and testing to be added next

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Aims
 
 - create a working stich counter for knitting or crochet projects.
 - link it via an API to an SQL database to allow the stich count to be saved for next time. 
+- write tests using vitest to test my application
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,28 @@
+import React, {useState} from 'react'
+
 function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    const add = () => {
+        setCount(prevCount => prevCount+1)
+    }
+
+    const subtract = () => {
+        setCount(prevCount => prevCount-1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+
     return ( 
     <>
     <h4>Number of stitches</h4>
-    <p>0</p>
-    <button>+</button>
-    <button>-</button>
-    <button>Reset</button>    
+    <p id='count-display'>{count}</p>
+    <button class='counter-button' id='add-button' onClick={add}>+</button>
+    <button class='counter-button' id='subtract-button' onClick={subtract}>-</button>
+    <button class='counter-button' id='reset-button' onClick={reset}>Reset</button>    
     </> );
 }
 
